37826f9b4e0580996fe2daddcdb981c9e611e140
ambre-lvd <126390963+ambre-lvd@users.noreply.github.com>
2025-06-21 12:05:55 +0800

    ajout du changement de nom

--- Python/app.py
+++ Python/app.py
@@ -16,20 +16,38 @@ def accueil():
 @app.route('/generateur', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        try:
-            nb_cotes = int(request.form['nb_cotes'])
-            profondeur = int(request.form['répétition'])
-            taille = int(request.form['taille'])
-            angle = float(request.form['angle'])
-            couleur = request.form['couleur']
-            filename = generer_motif(nb_cotes, profondeur, taille, angle, couleur)
-            session['image_path'] = url_for('static', filename=f'motifs/{filename}')
-            return redirect(url_for('index'))
-        except Exception as e:
-            return render_template('generateur.html', error=str(e))
-    
+        if 'nom_fichier_existant' in request.form:
+            ancien_nom = request.form['nom_fichier_existant']
+            nouveau_nom = request.form['nom_nouveau'].strip()
+            if ancien_nom and nouveau_nom:
+                dossier = os.path.join('static', 'motifs')
+                ancien_chemin = os.path.join(dossier, ancien_nom)
+                nouveau_fichier = f"{nouveau_nom}.png"
+                nouveau_chemin = os.path.join(dossier, nouveau_fichier)
+                if os.path.exists(ancien_chemin):
+                    os.rename(ancien_chemin, nouveau_chemin)
+                    session['image_path'] = url_for('static', filename=f'motifs/{nouveau_fichier}')
+                    session['generated_filename'] = nouveau_fichier
+                    return redirect(url_for('index'))
+        else:
+            try:
+                nb_cotes = int(request.form['nb_cotes'])
+                profondeur = int(request.form['répétition'])
+                taille = int(request.form['taille'])
+                angle = float(request.form['angle'])
+                couleur = request.form['couleur']
+                filename = generer_motif(nb_cotes, profondeur, taille, angle, couleur)
+                session['image_path'] = url_for('static', filename=f'motifs/{filename}')
+                session['generated_filename'] = filename
+                return redirect(url_for('index'))
+            
+            except Exception as e:
+                return render_template('generateur.html', error=str(e))
+
     image_path = session.pop('image_path', None)
-    return render_template('generateur.html', image_path=image_path)
+    generated_filename = session.pop('generated_filename', None)
+    return render_template('generateur.html', image_path=image_path, generated_filename=generated_filename)
+
 
 
 @app.route('/a-propos')
